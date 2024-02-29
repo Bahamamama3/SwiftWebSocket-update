dff9cd44871ad46bc2d3585bbd1fea6c8768f7f2
Bahamamama3 <90845553+Bahamamama3@users.noreply.github.com>
2024-02-29 12:50:39 +0600

    package updated

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version: 5.7
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftWebSocket"
+    name: "SwiftWebSocket",
+    products: [
+        .library(
+            name: "SwiftWebSocket",
+            targets: ["SwiftWebSocket"])
+    ],
+    targets: [
+        .target(
+            name: "SwiftWebSocket",
+            path: "Source")
+    ]
 )
